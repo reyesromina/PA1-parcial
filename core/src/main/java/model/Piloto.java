2ef789d5e264225c14062e14ac5cbb4202ecdfe3
Romina Reyes <rominareyes022@gmail.com>
2025-10-14 17:58:22 -0300

    Creacion Entidad Piloto + Exceptions

---
+++ core/src/main/java/model/Piloto.java
@@ -0,0 +1,37 @@
+package model;
+
+import exception.ExceptionMayorEdad;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.UUID;
+
+public class Piloto {
+    private UUID licencia;
+    private String nombre;
+    private LocalDate fechaNacto;
+    private String DNI;
+
+
+    private Piloto(UUID licencia, String nombre, LocalDate fechaNacto, String DNI) {
+        this.licencia = licencia;
+        this.nombre = nombre;
+        this.fechaNacto = fechaNacto;
+        this.DNI = DNI;
+    }
+
+    public static Piloto crearPiloto(UUID licencia, String nombre, LocalDate fechaNacto, String DNI){
+        //politicas mediante exceptios
+        if(Piloto.getEdad(fechaNacto)<18){
+            new ExceptionMayorEdad("El piloto debe ser mayor de edad");
+        }
+        return new Piloto(licencia,nombre,fechaNacto,DNI);
+    }
+
+    public static int getEdad(LocalDate fechaNacto){
+
+        return Period.between(fechaNacto,LocalDate.now()).getYears();
+    }
+
+}
